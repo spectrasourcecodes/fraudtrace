de8b9c10ef7867501edbc696dcb3da25def6bb4e
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-08-18 18:45:48 +0100

    Update UserWallet.jsx

--- src/pages/wallet/UserWallet.jsx
+++ src/pages/wallet/UserWallet.jsx
@@ -132,7 +132,7 @@ const UserWallet = () => {
                 <p className="text-xs text-gray-400">
                   {wallet.status === 'active'
                     ? 'Your wallet is active and you can make withdrawals.'
-                    : 'Your wallet is currently inactive.'}
+                    : 'Your wallet is currently inactive, please contact support for account reactivation.'}
                     {/* : 'Your wallet is inactive. Please make a one (1x) time deposit of $100 to reactivate your wallet and proceed with the withdrawal process.'} */}
                 </p>
               </div>
